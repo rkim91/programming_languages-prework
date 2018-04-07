107064726032bfd6ccd966b49dc7b4cfeff44105
Richard Kim <richeekim@gmail.com>
2018-04-07 00:27:29 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  newhash = {}
+  repeated_program = []
+  languages.each do |hash, language|
+    arr = []
+    arr << hash
+    repeated_program << hash
+    language.each do |program, type|
+      if newhash.has_key?(program)
+        newhash[program][:style] = repeated_program
+      else
+        newhash[program] = type
+        newhash[program][:style] = arr
+      end
+    end
+  end
+  return newhash
 end
